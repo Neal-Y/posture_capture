c297e1a47ac27c73e04b76ccc3a1ed8350218038
neal <neal0420yang@gmail.com>
2025-01-08 21:38:33 +0800

    update main.py

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,9 +1,12 @@
 from flask import Flask
-from pose_api import pose_analysis
+from app.api.pose_api import pose_api
+from app.api.health_api import health_api
 
+# 初始化 Flask 應用
 app = Flask(__name__)
-
-app.register_blueprint(pose_analysis)
+app.register_blueprint(pose_api)
+app.register_blueprint(health_api)
 
 if __name__ == "__main__":
+    # 啟動服務
     app.run(host="0.0.0.0", port=8000)
