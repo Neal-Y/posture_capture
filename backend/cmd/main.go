d1c87754c8a5c600b32cc439c27f240a3555fa72
neal <neal0420yang@gmail.com>
2025-01-09 11:55:10 +0800

    main.go 只負責初始化gin 服務

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"backend/internal/route"
 	"log"
-	"net/http"
 )
 
 func main() {
-	// 初始化 Gin 路由
-	router := route.InitRouter()
-
-	// 啟動伺服器
-	log.Println("Backend is running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// 啟動 Gin 服務
+	_, err := route.InitGinServer()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
