79da80b9059e47542bb2e016aaa0eebd5a6a5738
neal <neal0420yang@gmail.com>
2025-01-09 11:55:56 +0800

    gin.go 將route都集中在這處理，並且在這將需要用到的依賴初始化

--- backend/internal/route/gin.go
+++ backend/internal/route/gin.go
@@ -1,16 +1,34 @@
 package route
 
 import (
+	"backend/internal/clients"
 	"backend/internal/handlers"
+	"backend/internal/services"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
-// InitRouter initializes the API routes
-func InitRouter() *gin.Engine {
-	router := gin.Default()
+// InitGinServer 初始化 Gin 服務器
+func InitGinServer() (*gin.Engine, error) {
+	server := GinRouter()
+	err := server.Run("127.0.0.1:8080")
 
-	// 定義 /analyze 路由
-	router.POST("/analyze", handlers.AnalyzeHandler)
+	return server, err
+}
 
-	return router
+// GinRouter 註冊路由和初始化依賴
+func GinRouter() *gin.Engine {
+	server := gin.New()
+	server.Use(gin.Logger())
+
+	// 初始化依賴
+	client := clients.NewAIClient("http://localhost:8000", 10*time.Second)
+	analyzeService := services.NewAnalyzeService(client)
+
+	// 註冊處理器
+	handlers.AnalyzeService = analyzeService
+	api := server.Group("/api")
+	api.POST("/analyze", handlers.AnalyzeHandler)
+
+	return server
 }
