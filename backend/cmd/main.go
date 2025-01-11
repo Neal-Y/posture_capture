16b57bf49b31f64a78d92ef67213bb55908fdf2d
neal <neal0420yang@gmail.com>
2025-01-11 14:54:13 +0800

    feat(main) update

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"backend/configs"
 	"backend/internal/route"
 	"log"
 )
 
 func main() {
+	configs.LoadConfig()
+
 	// 啟動 Gin 服務
 	_, err := route.InitGinServer()
 	if err != nil {
