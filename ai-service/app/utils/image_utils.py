40d1e025c3b335e43776a51bc9a0c6f6fedebdf7
neal <neal0420yang@gmail.com>
2025-01-16 13:15:43 +0800

    feat(utils) 處理影片中”逐幀“的畫面

--- ai-service/app/utils/image_utils.py
+++ ai-service/app/utils/image_utils.py
@@ -3,9 +3,12 @@ import numpy as np
 
 def preprocess_image(image):
     """
-    將圖像轉換為 OpenCV 格式
+    Preprocess the image for pose detection.
     """
-    np_image = np.frombuffer(image.read(), np.uint8)
-    cv_image = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
-
-    return cv_image
+    if isinstance(image, np.ndarray):
+        # If the image is already a numpy array, use it directly
+        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    else:
+        # If the image is not a numpy array, read it as such
+        np_image = np.frombuffer(image.read(), np.uint8)
+        return cv2.imdecode(np_image, cv2.IMREAD_COLOR)
