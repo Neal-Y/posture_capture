4827b34e95dc87cabba1fcd586461cb88749a33b
neal <neal0420yang@gmail.com>
2025-02-03 16:28:28 +0800

    feat(ai-service/app) init.py

--- ai-service/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from .api.pose_api import pose_api
-from .api.health_api import health_api
-
-app = Flask(__name__)
-app.register_blueprint(pose_api)
-app.register_blueprint(health_api)
